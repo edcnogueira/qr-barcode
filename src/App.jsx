e6f5686d6a5b411128e632ef62f9f96bf91316b2
Edjan <edjan.nogueira14@gmail.com>
2022-08-20 10:52:47 -0300

    feat: insere componenete leitor.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner"
+
 function App() {
+  const [data, setData] = useState("Vazio")
 
   return (
-    <div>teste</div>
+    <div>
+      <BarcodeScannerComponent width={500} height={500} onUpdate={(err, result) => {
+          if (result) setData(result.text);
+          else setData("Not Found");
+        }}/>
+
+        <p>{data}</p>
+    </div>
   )
 }
 
