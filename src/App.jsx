5fd55ef9f0bcb84cc8a10c05ffebd1c573feb54f
Edjan <edjan.nogueira14@gmail.com>
2022-08-20 12:56:57 -0300

    feat: cria componente de leitura.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 // import BarcodeScannerComponent from "react-qr-barcode-scanner"
-import { LeitorDeCodigo } from "./components/leitorDeCodigo";
+import  LeitorDeCodigo  from "./components/leitorDeCodigo";
 
 function App() {
   const [data, setData] = useState("Vazio")
 
   return (
     <div>
-      <LeitorDeCodigo />
+      <LeitorDeCodigo width="300"  height="300" onUpdate={(err, result) => {
+          if (result) setData(result.text);
+          else setData("Not Found");
+        }}/>
 
         <p>{data}</p>
         <h1>teste2</h1>
